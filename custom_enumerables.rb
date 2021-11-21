23f69b7c82fefb778798d674a56d82544f192971
cjdepo <cjdepowell@protonmail.com>
2021-11-21 18:39:52 -0500

    each

--- custom_enumerables.rb
+++ custom_enumerables.rb
@@ -1,10 +1,19 @@
 module Enumerable
     def my_each
-        yield
+        if block_given?
+            for k, v in self
+                yield(k, v)
+            end
+        else
+            self.to_enum
+        end
     end
 end
 
 puts "my_each vs each"
 numbers = [1, 2, 3, 4, 5]
+hash_numbers = {a: 1, b: 2, c: 3, d: 4, e: 5}
 numbers.each { |item| puts item }
 numbers.my_each { |item| puts item }
+hash_numbers.each { |k, v| puts "#{k}, #{v}" }
+hash_numbers.my_each { |k, v| puts "#{k}, #{v}" }
